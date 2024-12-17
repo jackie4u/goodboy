16d423b10e2155388bea1047950bc921f919cad9
Jan Vackar <jackie4u@seznam.cz>
2024-12-17 23:44:11 +0100

    Implemented OfferService

--- src/GoodBoy.Api/Features/Offerings/OfferService.cs
+++ src/GoodBoy.Api/Features/Offerings/OfferService.cs
@@ -1,6 +1,67 @@
-﻿namespace GoodBoy.Api.Features.Offerings
+﻿using System.Collections.Concurrent;
+using GoodBoy.Api.Persistence.Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.IdentityModel.Tokens;
+
+namespace GoodBoy.Api.Features.Offerings;
+
+public class OfferService
 {
-    public class OfferService
+    private readonly ConcurrentDictionary<int, Offer> _offerings = new();
+
+    public OfferService()
     {
+    }
+
+    public Task<Offer?> GetByIdAsync(int id)
+    {
+        return Task.FromResult(_offerings.GetValueOrDefault(id));
+    }
+
+    public Task<IEnumerable<Offer>> GetAllAsync()
+    {
+        return Task.FromResult<IEnumerable<Offer>>(_offerings.Values);
+    }
+
+    public async Task<Offer> CreateAsync(Offer offer)
+    {
+
+        //if (1 == 2)
+        //{
+        //    var error = "This newOffer already exists in the system";
+        //    retunr 
+        //}
+
+        _offerings[offer.Id] = offer;
+
+        return offer;
+    }
+
+    public async Task<Offer> UpdateAsync(Offer newOffer)
+    {
+        //Todo <medium>: Reimplement
+        if (_offerings.TryGetValue(newOffer.Id, out Offer oldOffer))
+        {
+            // Key found, return the Offer
+            // return Result.Ok(newOffer);
+        } else
+        {
+            // Key not found
+            // return Result.Fail<Offer, string>($"No newOffer found with ID: {id}");
+        }
+
+        _offerings[newOffer.Id] = newOffer;
+        return newOffer;
+    }
+
+    public async Task<bool> DeleteByIdAsync(int id)
+    {
+        _offerings.Remove(id, out var offer);
+        if (offer is null)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
--- src/GoodBoy.Api/Persistence/Entities/Offer.cs
+++ src/GoodBoy.Api/Persistence/Entities/Offer.cs
@@ -2,7 +2,7 @@ namespace GoodBoy.Api.Persistence.Entities;
 
 public class Offer
 {
-    public int Id { get; set; }
+    public required int Id { get; set; }
     // public string Ean { get; set; }
     public string Name { get; set; } = string.Empty;
     public string? Description { get; set; }
